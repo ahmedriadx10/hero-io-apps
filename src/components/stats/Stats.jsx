fe5e3aade0ded5271abb687115897ebbdd73c00e
ahmedriadx10 <infinitecoderx02@gmail.com>
2026-04-13 20:02:02 +0600

    stats section added

--- src/components/stats/Stats.jsx
+++ src/components/stats/Stats.jsx
@@ -1,9 +1,33 @@
 
 const Stats = () => {
   return (
-    <div>
+    <section className="py-20 bg-linear-[160deg,#632EE3,#9F62F2]">
       
-    </div>
+<div className="w-[90%] mx-auto">
+  <h4 className="text-center text-4xl lg:text-5xl text-white font-bold">Trusted by Millions, Built for You</h4>
+</div>
+
+<div className="max-w-7xl mx-auto text-center w-[90%] grid grid-cols-3 mt-10 gap-5">
+
+<div className="space-y-2  text-white">
+  <p className="text-[12px] sm:text-[16px]">Total Downloads</p>
+<h4 className="text-2xl sm:text-5xl lg:text-[64px] font-extrabold">29.6M</h4>
+<p className="text-[12px] sm:text-[16px]">21% More than Last Month</p>
+</div>
+<div className="space-y-2   text-white">
+  <p className="text-[12px] sm:text-[16px]">Total Reviews</p>
+<h4 className="text-2xl sm:text-5xl lg:text-[64px] font-extrabold">906K</h4>
+<p className="text-[12px] sm:text-[16px]">46% more than last month</p>
+</div>
+<div className="space-y-2  text-white">
+  <p className="text-[12px] sm:text-[16px]">Active Apps</p>
+<h4 className="text-2xl sm:text-5xl lg:text-[64px] font-extrabold">132+</h4>
+<p className="text-[12px] sm:text-[16px]">31 more will Launch</p>
+</div>
+
+</div>
+
+    </section>
   );
 };
 
